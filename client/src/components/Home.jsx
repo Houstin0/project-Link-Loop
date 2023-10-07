36136f5635a983e3f9714e2c75697d97f4fad995
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-10-08 00:33:43 +0300

    added style to frontend

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,15 +17,28 @@ function Home() {
     fetch('api/comments').then(res => res.json()).then(data=>setComments(data))
   },[])
 
-  return (
-    <>
-      <div id="posts-container">
-        {posts.map((post) => (
-          <div key={post.id} className="post">
-            <img src={post.image_url} alt={post.caption} />
+return (
+  <div id="post_container">
+    {posts.map((post) => (
+      <div
+        key={post.id}
+        className="custom-max-width p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div className="flex">
+          <div className="w-1/2">
+              <img
+                className="rounded-t-lg object-cover  w-full h-full"
+                src={post.image_url}
+                alt={post.caption}
+              />
+          </div>
+          <div className="w-1/2">
             <div className="post-details">
               <div className="post-user-info">
-                <img src={post.user.profile_picture_url} alt={post.user.username} />
+                <img
+                  src={post.user.profile_picture_url}
+                  alt={post.user.username}
+                />
                 <span>{post.user.username}</span>
               </div>
               <div className="post-caption">{post.caption}</div>
@@ -33,7 +46,7 @@ function Home() {
                 <span>{post.likes} likes</span>
               </div>
               <div className="post-comments">
-              <div className="comments-title">Comments:</div>
+                <div className="comments-title">comments:</div>
                 {comments
                   .filter((comment) => comment.post_id === post.id)
                   .slice(0, visibleComments[post.id] ? comments.length : 3)
@@ -45,17 +58,18 @@ function Home() {
                   ))}
               </div>
               <button onClick={() => toggleComments(post.id)}>
-                {visibleComments[post.id] ? 'Hide' : 'More.....'}
+                {visibleComments[post.id] ? 'Hide comments..' : 'More comments...'}
               </button>
-              <div className='post-date'>
+              <div className="post-date">
                 <span>{post.created_at}</span>
               </div>
             </div>
           </div>
-        ))}
+        </div>
       </div>
-    </>
-  )
+    ))}
+  </div>
+);
 }
 
 export default Home
