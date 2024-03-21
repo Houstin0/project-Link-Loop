95c9bc04e6b1320fad7f83d9b5441eae0085fbc9
Houstin0 <houstin0000@gmail.com>
2024-03-21 23:22:54 +0300

    update2

--- server/app/main.py
+++ server/app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
-from  app.database import engine
+from app.database import engine
 from app.models import Base
 from app.routes import users,login,posts,messages,comments,friends
 
-app=FastAPI()
+app = FastAPI(docs_url="/")
 
 Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Welcome to Link Loop backend"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "Welcome to Link Loop backend"}
 
 app.include_router(login.router)
 app.include_router(users.router)
