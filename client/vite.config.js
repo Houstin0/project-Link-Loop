88a290a46b3e7ea2319b7fa428ff7691b6908e54
Houstin0 <houstin0000@gmail.com>
2024-04-05 18:10:47 +0300

    new postgres added to client

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
       proxy: {
         '/api': {
-          target: 'https://link-db.onrender.com',
+          target: 'https://link-loop-db.onrender.com',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
